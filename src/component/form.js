fc4dc2bdd4a90254fcac759c586d4d0a7386db4c
ylu021 <yichen_0@hotmail.com>
2017-06-28 23:02:08 -0400

    signup form validation

--- src/component/form.js
+++ src/component/form.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Form, FormGroup, Label, Input } from "reactstrap"
+import { Form, FormGroup, FormFeedback, Label, Input } from "reactstrap"
+import PropTypes from 'prop-types'
 
 export const CusForm = (props) => (
   <Form>
@@ -8,11 +9,20 @@ export const CusForm = (props) => (
 )
 
 export const FormRow = (props) => {
-  const {label, inputtype} = props
+  const {name, label, inputtype, onChange, error} = props
+  const state = !error? null : 'danger'
+  const tooltip = ' (minimum length 8)'
   return (
-    <FormGroup>
-      <Label htmlFor={label}>{label}</Label>
-      <Input type={inputtype} name={label} />
+    <FormGroup color={state}>
+      <Label htmlFor={name} >{name==='password'? label+tooltip : label}</Label>
+      <Input type={inputtype} name={name} onChange={onChange} state={state} />
+      {error? <FormFeedback>Invalid {name}</FormFeedback> : null}
+
     </FormGroup>
   )
+}
+
+FormRow.PropTypes = {
+  label: PropTypes.string,
+  inputtype: PropTypes.string,
 }
--- src/signup.js
+++ src/signup.js
@@ -6,9 +6,65 @@ import Feed from "./feed";
 import {fakeAuth} from "./route";
 import CusButton from './component/button'
 import { CusForm, FormRow } from './component/form'
+import _ from 'lodash'
 
 class Signup extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      formValid: false,
+      email: '',
+      password: '',
+      passwordc: '',
+      formErrors: {
+        email: false,
+        password: false,
+        passwordc: false,
+      },
+    }
+  }
+  handleInput = (e) => {
+    const {name, value} = e.target
+    this.setState({
+      [name]: value.trim(),
+    })
+    this.validateField(name, value)
+  }
+  validateField = (name, value) => {
+    let formErrors = this.state.formErrors
+    console.log(formErrors);
+    switch(name) {
+      case 'email':
+        formErrors['email'] = !(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i).test(value)
+        break
+      case 'password':
+        formErrors['password'] = !(value.length >= 8)
+        break
+      case 'passwordc':
+        formErrors['passwordc'] = !(this.state.password === value)
+      default:
+        break
+    }
+    this.setState({
+      formErrors: formErrors,
+    })
+
+    this.validateForm()
+  }
+  validateForm = () => {
+    const formErrors = {
+      email: false,
+      password: false,
+      passwordc: false,
+    }
+    if(this.state.email && this.state.password && this.state.passwordc) {
+      this.setState({
+        formValid: _.isEqual(formErrors, this.state.formErrors),
+      })
+    }
+  }
   render() {
+    const {email, password, passwordc} = this.state.formErrors
     return (
       <div>
         <Header user="" logined={false}/>
@@ -17,10 +73,10 @@ class Signup extends Component {
           <div className="row">
             <div className="col-8 mx-auto cusform">
               <CusForm>
-                <FormRow label="email" inputtype="email"/>
-                <FormRow label="password" inputtype="password"/>
-                <FormRow label="Confirm password" inputtype="password"/>
-                <CusButton color="primary">Submit</CusButton>
+                <FormRow label="email" name="email" inputtype="email" onChange={this.handleInput} error={email}/>
+                <FormRow label="password" name="password" inputtype="password" onChange={this.handleInput} error={password} />
+                <FormRow label="Confirm password" name="passwordc" inputtype="password" onChange={this.handleInput} error={passwordc} />
+                <CusButton color="primary" disabled={!this.state.formValid}>Submit</CusButton>
               </CusForm>
             </div>
           </div>
