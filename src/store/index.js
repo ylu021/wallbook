1496f10d9f17984b660da096dc12aa799e0fbdd1
ylu021 <yichen_0@hotmail.com>
2017-07-03 10:58:50 -0400

    adding middleware redux-promise redux-thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,29 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
+import promiseMiddleware from 'redux-promise-middleware'
+import thunkMiddleware from 'redux-thunk'
+
+const loggerMiddleware = () => {
+  return next => action => {
+    if (process.env.NODE_ENV !== 'production') {
+      const { type, payload, meta } = action;
+
+      console.groupCollapsed(type);
+      console.log('Payload:', payload);
+      console.log('Meta:', meta);
+      console.groupEnd();
+    }
+
+    return next(action);
+  }
+}
 
 export default(initState) => {
-  return createStore(rootReducer, initState)
+  return createStore(rootReducer, initState, applyMiddleware(
+    thunkMiddleware,
+    promiseMiddleware({
+      promiseTypeSuffixes: ['LOADING', 'SUCCESS', 'ERROR']
+    }),
+    loggerMiddleware
+  ))
 }
