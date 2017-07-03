437b135db9596cbeff09e58f8fc09aea9d8880c9
ylu021 <yichen_0@hotmail.com>
2017-07-03 10:57:42 -0400

    changing api data

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 const fakeusers = [
-  {name: 'faker', password: '12345678'},
-  {name: 'faker2', password: '12345678'},
-  {name: 'faker3', password: '12345678'},
+  {email: 'faker@gmail.com', password: '12345678'},
+  {email: 'faker2@gmail.com', password: '12345678'},
+  {email: 'faker3@gmail.com', password: '12345678'},
 ]
 
 export default () => {
   return new Promise((resolve, rej) => {
     setTimeout(() => {
       return resolve(fakeusers)
-    }, 3000)
+    }, 1000)
   })
 }
