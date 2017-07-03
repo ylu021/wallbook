3d3af2255ba6f2d772451815f1505b13d09c8446
ylu021 <yichen_0@hotmail.com>
2017-07-03 11:00:10 -0400

    async action reducer handling

--- src/actions/useraction.js
+++ src/actions/useraction.js
@@ -1,8 +1,22 @@
 import types from '../constant'
+import getUsers from '../api'
 
-export const addUser = (user) => (
-  {
+export const addUser = (user) => {
+  console.log('calling action add user')
+  return {
     type: types.ADD_USER,
-    user
+    payload: {
+        promise: getUsers(), // a promise
+        user: user // optional data object for optimistic update
+    }
   }
-)
+}
+// (
+//   {
+//     type: types.ADD_USER,
+//     user: {
+//         promise: getUsers(), // a promise
+//         data: user // optional data object for optimistic update
+//     }
+//   }
+// )
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,12 +1,27 @@
 import types from '../constant'
 
-const initialState = {}
+const initialState = {
+  isFetching: false,
+  error: false,
+  user: []
+}
 export default(state = initialState, action) => {
   switch(action.type) {
-    case types.ADD_USER:
+    case `${types.ADD_USER}_LOADING`:
+      console.log('loading')
       return {
-        ...state,
-        user: action.user
+        user: [],
+        isFetching: true
+      }
+    case `${types.ADD_USER}_SUCCESS`:
+      return {
+        user: action.payload,
+        isFetching: false,
+      }
+    case `${types.ADD_USER}_ERROR`:
+      return {
+        error: action.payload,
+        isFetching: false,
       }
     default:
       return state
