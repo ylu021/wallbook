abff914dd1b8f74f11e4280e05ae9868e1e3a28e
ylu021 <yichen_0@hotmail.com>
2017-07-03 11:00:32 -0400

    testing action reducer logic working

--- src/signup.js
+++ src/signup.js
@@ -45,10 +45,36 @@ class Signup extends Component {
   onPaste = (e) => {
     e.preventDefault()
   }
+
+  signup = (e) => {
+    e.preventDefault()
+    const { email, password } = this.state
+    console.log('im here')
+
+    this.props.actions.addUser({
+      email,
+      password
+    })
+  }
+
   render() {
     const {email, password, passwordc} = this.state.formErrors
+    const { isFetching, error, user } = this.props
     return (
       <div>
+        <span>Something should happen {
+          isFetching? 'LOADING User': (
+            !error? 'DONE': 'ERROR'
+          )
+        }</span>
+        {user.length > 0 ? (
+          <ul>
+            {user.map((u, idx) => (
+              <li key={idx}>{u.email}-{u.password}</li>
+            ))}
+          </ul>
+          ) : null
+        }
         <Header user="" logined={false}/>
         <section id="signup" className="container">
           <h1 className="text-center">Sign Up</h1>
@@ -58,7 +84,7 @@ class Signup extends Component {
                 <FormRow label="email" name="email" inputtype="email" onChange={this.handleInput} error={email}/>
                 <FormRow label="password" name="password" inputtype="password" onChange={this.handleInput} error={password} />
                 <FormRow onPaste={this.onPaste} label="Confirm password" name="passwordc" inputtype="password" onChange={this.handleInput} error={passwordc} />
-                <CusButton color="primary" disabled={!this.state.formValid} className="btn-submit">Submit</CusButton>
+                <CusButton color="primary" disabled={!this.state.formValid} className="btn-submit" onClick={this.signup}>Submit</CusButton>
               </CusForm>
             </div>
           </div>
@@ -68,17 +94,20 @@ class Signup extends Component {
   }
 }
 
-const mapStateToProps = (state, props) => (
-  // state from store to props
-  {
-    users: state.users
+const mapStateToProps = (state, props) => {
+    // state from store to props
+  console.log(state.users)
+  return {
+    user: state.users.user,
+    isFetching: state.users.isFetching,
+    error: state.users.error
   }
-)
+}
 
 const mapDispatchToProps = (dispatch) => (
   // action from dispatch to store
   {
-    action: bindActionCreators(Actions, dispatch)
+    actions: bindActionCreators(Actions, dispatch)
   }
 )
 
