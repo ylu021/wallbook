2d0f39b1dbd8ed3f7f866a6af4ec86038cbe849e
ylu021 <yichen_0@hotmail.com>
2017-07-11 23:17:28 -0400

    spacing

--- src/reducers/sessions.js
+++ src/reducers/sessions.js
@@ -29,23 +29,23 @@ export default(state = initialState, action) => {
         error: action.payload,
         fetched: action.payload.fetched,
       }
-      case `${types.ADD_POST}_LOADING`:
-        return {
-          user: state.user,
-          isAdding: true,
-        }
-      case `${types.ADD_POST}_SUCCESS`:
-        return {
-          user: state.user,
-          isAdding: false,
-          added: action.payload.added
-        }
-      case `${types.ADD_POST}_ERROR`:
-        return {
-          isAdding: false,
-          error: action.payload,
-          added: action.payload.added,
-        }
+    case `${types.ADD_POST}_LOADING`:
+      return {
+        user: state.user,
+        isAdding: true,
+      }
+    case `${types.ADD_POST}_SUCCESS`:
+      return {
+        user: state.user,
+        isAdding: false,
+        added: action.payload.added
+      }
+    case `${types.ADD_POST}_ERROR`:
+      return {
+        isAdding: false,
+        error: action.payload,
+        added: action.payload.added,
+      }
     default:
       return state
   }
