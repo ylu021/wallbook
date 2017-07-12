87da27071415e6ada982a89fa6c3217bd76ea58c
ylu021 <yichen_0@hotmail.com>
2017-07-12 12:37:34 -0400

    add tag component

---
+++ src/component/tag.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const tag = styled.span`
+    background: #FF334B;
+    padding: 0.5rem 1rem;
+    color: white;
+    font-weight: 500;
+    font-size: 1.2rem;
+    border-radius: 3px;
+`
+
+export default tag
